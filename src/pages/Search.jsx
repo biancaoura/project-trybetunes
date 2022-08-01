33cf31b951fde4ceee7f7741eb0186a00dc81120
Bianca Oura <biancaoura@gmail.com>
2022-08-01 19:20:21 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,46 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchInput: '',
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { searchInput } = this.state;
+    const { handleChange } = this;
+
+    const minLength = 2;
+    const hasMinChar = searchInput.length >= minLength;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <section>
+          <input
+            type="text"
+            id="searchInput"
+            name="searchInput"
+            placeholder="Banda / Artista"
+            value={ searchInput }
+            onChange={ handleChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="button"
+            disabled={ !hasMinChar }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </section>
         Search
       </div>
     );
