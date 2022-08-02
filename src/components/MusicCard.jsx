31f7721a654710485f80edae23e5d666e0133d01
Bianca Oura <biancaoura@gmail.com>
2022-08-02 14:13:59 -0300

    Requisito 7

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class MusicCard extends Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+
+    return (
+      <li>
+        <section>
+          <span>{trackName}</span>
+          <audio data-testid="audio-component" src={ previewUrl } controls>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            <code>audio</code>
+          </audio>
+        </section>
+      </li>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+};
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
+import PropTypes, { objectOf } from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 export default class Album extends Component {
+  state = {
+    allSongs: [],
+    loading: true,
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: {
+          id,
+        } } } = this.props;
+
+    const data = await getMusics(id);
+    this.setState({
+      allSongs: data,
+      loading: false,
+    });
+  }
+
   render() {
+    const { allSongs, loading } = this.state;
+    const { collectionName, artistName } = allSongs[0] || {};
+    console.log(allSongs);
+
     return (
       <div data-testid="page-album">
         <Header />
-        Album
+
+        <section>
+          <h4 data-testid="album-name">{collectionName}</h4>
+          <p data-testid="artist-name">{artistName}</p>
+        </section>
+
+        <ul>
+          {
+            loading
+              ? <h2>Carregando...</h2>
+              : (
+                allSongs
+                  .slice(1)
+                  .map((song) => (
+                    <MusicCard key={ song.trackId } { ...song } />
+                  ))
+
+              )
+          }
+        </ul>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: objectOf(PropTypes.string),
+  }).isRequired,
+};
