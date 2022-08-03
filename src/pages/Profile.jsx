9ee24317fc3ba015b6099e19ff725846b11ca6ab
Bianca Oura <biancaoura@gmail.com>
2022-08-03 19:02:42 -0300

    Requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  state = {
+    name: '',
+    email: '',
+    description: '',
+    image: '',
+    loading: true,
+  }
+
+  async componentDidMount() {
+    const userInfo = await getUser();
+
+    Object.entries(userInfo).forEach(([key, value]) => {
+      this.setState({
+        [key]: value,
+        loading: false,
+      });
+    });
+  }
+
   render() {
+    const { name, email, description, image, loading } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        {
+          loading
+            ? <h4>Carregando...</h4>
+            : (
+              <section>
+                <h3>{name}</h3>
+                <p>{email}</p>
+                <p>{description}</p>
+                <img src={ image } alt="imagem de perfil" data-testid="profile-image" />
+                <Link to="/profile/edit">Editar perfil</Link>
+              </section>
+            )
+        }
       </div>
     );
   }
