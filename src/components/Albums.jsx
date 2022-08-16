74eef3b9033d592d4aad73d3be80a6d7179e6903
Bianca Oura <biancaoura@gmail.com>
2022-08-16 20:55:13 -0300

    Estiliza

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -9,9 +9,9 @@ export default class Albums extends Component {
 
     if (albums.length < 1) return <p>No results found</p>;
     return (
-      <section>
-        <h3>{`Album results for: ${artist}`}</h3>
-        <ul>
+      <section className={ styles.outer_container }>
+        <h3 className={ styles.h3 }>{`Album results for: ${artist}`}</h3>
+        <ul className={ styles.cards_container }>
           {
             albums.map(({
               artistName,
@@ -19,15 +19,19 @@ export default class Albums extends Component {
               collectionId,
               artworkUrl100,
             }) => (
-              <li key={ collectionId }>
-                <img src={ artworkUrl100 } alt={ `${collectionName} cover` } />
-                <h4>{collectionName}</h4>
-                <p>{artistName}</p>
+              <li key={ collectionId } className={ styles.card }>
                 <Link
                   to={ `/album/${collectionId}` }
+                  className={ styles.link }
                   data-testid={ `link-to-album-${collectionId}` }
                 >
-                  More info
+                  <img
+                    src={ artworkUrl100 }
+                    alt={ `${collectionName} cover` }
+                    className={ styles.img }
+                  />
+                  <h4 className={ styles.h4 }>{collectionName}</h4>
+                  <p className={ styles.p }>{artistName}</p>
                 </Link>
               </li>
             ))
