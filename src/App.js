49024cba617a873bed67e9effe48beff4dc5e978
Bianca Oura <biancaoura@gmail.com>
2022-08-20 14:08:13 -0300

    rm browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { React, Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -8,22 +8,18 @@ import ProfileEdit from './pages/ProfileEdit';
 import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="/profile" component={ Profile } />
-          <Route component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/profile" component={ Profile } />
+        <Route component={ NotFound } />
+      </Switch>
     );
   }
 }
-
-export default App;
